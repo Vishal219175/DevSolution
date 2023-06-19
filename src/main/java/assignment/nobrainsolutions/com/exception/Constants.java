1af4f4bc9dab97680173185d48d52f66a4c1b6f7
Gajanan Bhawale <gajanan@symbiotiktech.com>
2023-06-19 11:08:34 +0530

    dev commit 7

--- src/main/java/assignment/nobrainsolutions/com/exception/Constants.java
+++ src/main/java/assignment/nobrainsolutions/com/exception/Constants.java
@@ -32,5 +32,5 @@ public class Constants {
      */
     public static final String NO_SEARCH_CRITERIA_PROVIDED = "No search criteria provided";
 }
-}
+
 
--- src/main/java/assignment/nobrainsolutions/com/exception/ErrorResponse.java
+++ src/main/java/assignment/nobrainsolutions/com/exception/ErrorResponse.java
@@ -2,7 +2,6 @@ package assignment.nobrainsolutions.com.exception;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
 
 
 /**
